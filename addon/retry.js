526b673403b921f806515bf4b695ca236472ee49
mrloop <ewan@mrloop.com>
2018-12-01 20:02:24 +0000

    Simplify

--- addon/retry.js
+++ addon/retry.js
@@ -3,17 +3,19 @@ import { later } from '@ember/runloop';
 import { isNone } from '@ember/utils';
 import { Promise as EmberPromise, reject } from 'rsvp';
 
-let retry = function (delayArg){
+let retry = function (fncToRetry, delayArg, conditionFnc){
 
   let r = {
-    retryIt: function(fnc, maxRetries=5, retries, conditionFunc){
+    retryIt: function(maxRetries=5, retries){
       if(retries >= maxRetries){
-        return r.asPromise(fnc);
+        return r.asPromise(r.fncToRetry);
       } else {
-        return r.retryLater(fnc, maxRetries, retries, conditionFunc);
+        return r.retryLater(maxRetries, retries);
       }
     },
 
+    fncToRetry: null,
+    conditionFnc: null,
     delayFnc: null,
     delay: 500,
 
@@ -21,44 +23,30 @@ let retry = function (delayArg){
       return Math.pow(2, retries) * r.delay;
     },
 
-    retryLater: function(fnc, maxRetries, retries, conditionFunc){
-      return new EmberPromise((resolve, reject)=>{
-        r.asPromise(fnc).then((result)=>{
-          resolve(result);
-        }).catch((error)=>{
-          if (r.isFunc(conditionFunc)) {
-            if (conditionFunc(error)) {
-              // If conditionFunc allows the retry then retry, otherwise reject
-              // e.g. In case of a 401 and the person doesn't want to retry
-              r.delayedRetry(fnc, maxRetries, retries, conditionFunc, resolve, reject)
-            } else {
-              reject(error);
-            }
-          } else {
-            // If the conditionFunc isn't passed, it will by default go here
-            r.delayedRetry(fnc, maxRetries, retries, conditionFunc, resolve, reject)
-          }
-        });
+    retryLater: function(maxRetries, retries){
+      return r.asPromise(r.fncToRetry).catch((error) => {
+        if (r.conditionFnc(error)) {
+          // If conditionFunc allows the retry then retry, otherwise reject
+          // e.g. In case of a 401 and the person doesn't want to retry
+          return r.delayedRetry(maxRetries, retries)
+        } else {
+          throw error;
+        }
       });
     },
 
     // Function that retries in case of failure
-    delayedRetry: function(fnc, maxRetries, retries, conditionFunc, resolve, reject) {
-      later(()=>{
-        r.retryIt(fnc, maxRetries, retries + 1, conditionFunc).then((result)=>{
-          resolve(result);
-        }, (error)=> {
-          reject(error);
-        });
-      }, r.delayFnc(retries));
+    delayedRetry: function(maxRetries, retries) {
+      return new EmberPromise(resolve => {
+        later(
+          () => resolve(r.retryIt(maxRetries, retries + 1)),
+          r.delayFnc(retries)
+        );
+      });
     },
 
     isPromise: function(obj){
-      return isPresent(obj) && r.isFunc(obj.then);
-    },
-
-    isFunc: function(obj){
-      return isPresent(obj) && typeOf(obj) === 'function';
+      return isPresent(obj) && typeOf(obj.then) === 'function';
     },
 
     asPromise: function(fnc){
@@ -66,7 +54,7 @@ let retry = function (delayArg){
         try {
           let returnVal = fnc(resolve, reject);
           if(r.isPromise(returnVal)){ // handle promise returned
-            returnVal.then((result)=> resolve(result), (error)=> reject(error));
+            returnVal.then(resolve, reject);
           }
           else if (!isNone(returnVal)) { // handle scalar value returned
             resolve(returnVal);
@@ -87,14 +75,16 @@ let retry = function (delayArg){
       }
     }
   }
+  r.fncToRetry = fncToRetry;
+  r.conditionFnc = conditionFnc;
   r.setDelayArg(delayArg);
   return r;
 }
 
-export default function(fnc, maxRetries=5, delayArg, conditionFunc) {
+export default function(fnc, maxRetries=5, delayArg, conditionFnc = () => true) {
   if(fnc === null || fnc === undefined || typeOf(fnc) !== 'function'){
     return reject('Function required');
   } else {
-    return retry(delayArg).retryIt(fnc, maxRetries, 0, conditionFunc);
+    return retry(fnc, delayArg, conditionFnc).retryIt(maxRetries, 0);
   }
 }
