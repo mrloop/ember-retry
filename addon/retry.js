be159b7bc3a1de6fd1379f95072f6adb81396bd7
mrloop <ewan@mrloop.com>
2021-01-24 10:01:13 +0000

    fix: js linting
    Run `yarn lint:js:fix`

--- addon/retry.js
+++ addon/retry.js
@@ -3,11 +3,10 @@ import { later } from '@ember/runloop';
 import { isNone } from '@ember/utils';
 import { Promise as EmberPromise, reject } from 'rsvp';
 
-let retry = function (fncToRetry, delayArg, conditionFnc){
-
+let retry = function (fncToRetry, delayArg, conditionFnc) {
   let r = {
-    retryIt: function(maxRetries=5, retries){
-      if(retries >= maxRetries){
+    retryIt: function (maxRetries = 5, retries) {
+      if (retries >= maxRetries) {
         return r.asPromise(r.fncToRetry);
       } else {
         return r.retryLater(maxRetries, retries);
@@ -19,16 +18,16 @@ let retry = function (fncToRetry, delayArg, conditionFnc){
     delayFnc: null,
     delay: 500,
 
-    exponentialDelayFnc: function(retries){
+    exponentialDelayFnc: function (retries) {
       return Math.pow(2, retries) * r.delay;
     },
 
-    retryLater: function(maxRetries, retries){
+    retryLater: function (maxRetries, retries) {
       return r.asPromise(r.fncToRetry).catch((error) => {
         if (r.conditionFnc(error)) {
           // If conditionFunc allows the retry then retry, otherwise reject
           // e.g. In case of a 401 and the person doesn't want to retry
-          return r.delayedRetry(maxRetries, retries)
+          return r.delayedRetry(maxRetries, retries);
         } else {
           throw error;
         }
@@ -36,8 +35,8 @@ let retry = function (fncToRetry, delayArg, conditionFnc){
     },
 
     // Function that retries in case of failure
-    delayedRetry: function(maxRetries, retries) {
-      return new EmberPromise(resolve => {
+    delayedRetry: function (maxRetries, retries) {
+      return new EmberPromise((resolve) => {
         later(
           () => resolve(r.retryIt(maxRetries, retries + 1)),
           r.delayFnc(retries)
@@ -45,44 +44,50 @@ let retry = function (fncToRetry, delayArg, conditionFnc){
       });
     },
 
-    isPromise: function(obj){
+    isPromise: function (obj) {
       return isPresent(obj) && typeOf(obj.then) === 'function';
     },
 
-    asPromise: function(fnc){
-      return new EmberPromise((resolve, reject)=>{
+    asPromise: function (fnc) {
+      return new EmberPromise((resolve, reject) => {
         try {
           let returnVal = fnc(resolve, reject);
-          if(r.isPromise(returnVal)){ // handle promise returned
+          if (r.isPromise(returnVal)) {
+            // handle promise returned
             returnVal.then(resolve, reject);
-          }
-          else if (!isNone(returnVal)) { // handle scalar value returned
+          } else if (!isNone(returnVal)) {
+            // handle scalar value returned
             resolve(returnVal);
           }
           // handle no value returned, will be handle by resolve, reject being called in fnc
-        } catch(error) {
+        } catch (error) {
           reject(error);
         }
       });
     },
 
-    setDelayArg: function(delayArg){
+    setDelayArg: function (delayArg) {
       r.delayFnc = r.exponentialDelayFnc;
-      if(typeOf(delayArg) === 'number'){
+      if (typeOf(delayArg) === 'number') {
         r.delay = delayArg;
-      }else if(typeOf(delayArg) === 'function'){
+      } else if (typeOf(delayArg) === 'function') {
         r.delayFnc = delayArg;
       }
-    }
-  }
+    },
+  };
   r.fncToRetry = fncToRetry;
   r.conditionFnc = conditionFnc;
   r.setDelayArg(delayArg);
   return r;
-}
+};
 
-export default function(fnc, maxRetries=5, delayArg, conditionFnc = () => true) {
-  if(fnc === null || fnc === undefined || typeOf(fnc) !== 'function'){
+export default function (
+  fnc,
+  maxRetries = 5,
+  delayArg,
+  conditionFnc = () => true
+) {
+  if (fnc === null || fnc === undefined || typeOf(fnc) !== 'function') {
     return reject('Function required');
   } else {
     return retry(fnc, delayArg, conditionFnc).retryIt(maxRetries, 0);
--- tests/unit/retry-test.js
+++ tests/unit/retry-test.js
@@ -3,171 +3,195 @@ import retry from 'dummy/retry';
 import { module, test } from 'qunit';
 import { later } from '@ember/runloop';
 
-module('Unit | Utility | retry', function() {
-
-  test('it works with function', function(assert) {
+module('Unit | Utility | retry', function () {
+  test('it works with function', function (assert) {
     let done = assert.async();
-    retry((resolve)=> {
-      resolve(7);
-    }, 3, ()=>{ return 2;}).then((result)=>{
-      assert.equal(result, 7);
-      done();
-    }).catch((error)=>{
-      assert.ok(false, error);
-      done();
-    });
+    retry(
+      (resolve) => {
+        resolve(7);
+      },
+      3,
+      () => {
+        return 2;
+      }
+    )
+      .then((result) => {
+        assert.equal(result, 7);
+        done();
+      })
+      .catch((error) => {
+        assert.ok(false, error);
+        done();
+      });
   });
 
-  test('it work with function that throws', function(assert){
+  test('it work with function that throws', function (assert) {
     let done = assert.async();
-    retry(()=>{
-      throw "I'm throwing";
-    }, 3, 1).then((result)=>{
-      assert.ok(false, result);
-      done();
-    }).catch((error)=>{
-      assert.equal("I'm throwing", error);
-      done();
-    });
+    retry(
+      () => {
+        throw "I'm throwing";
+      },
+      3,
+      1
+    )
+      .then((result) => {
+        assert.ok(false, result);
+        done();
+      })
+      .catch((error) => {
+        assert.equal("I'm throwing", error);
+        done();
+      });
   });
 
-  test('it rejects when no function', function(assert) {
+  test('it rejects when no function', function (assert) {
     let done = assert.async();
-    retry(undefined, 5, ()=>{return 2;}).then((result)=>{
-      assert.ok(false, result);
-      done();
-    }).catch((error)=>{
-      assert.equal(error.trim(), 'Function required');
-      done();
-    });
+    retry(undefined, 5, () => {
+      return 2;
+    })
+      .then((result) => {
+        assert.ok(false, result);
+        done();
+      })
+      .catch((error) => {
+        assert.equal(error.trim(), 'Function required');
+        done();
+      });
   });
 
-  test('it works with function returning a promise', function(assert) {
+  test('it works with function returning a promise', function (assert) {
     let done = assert.async();
-    retry(()=>{
+    retry(() => {
       return resolve('success');
-    }).then((result)=>{
-      assert.equal(result, 'success');
-      done();
-    }).catch((error)=>{
-      assert.ok(false, error);
-      done();
-    });
+    })
+      .then((result) => {
+        assert.equal(result, 'success');
+        done();
+      })
+      .catch((error) => {
+        assert.ok(false, error);
+        done();
+      });
   });
 
-  test('it works with function returning a scalar', function(assert) {
+  test('it works with function returning a scalar', function (assert) {
     let done = assert.async();
     retry(() => {
       return 'success';
-    }).then((result)=>{
-      assert.equal(result, 'success');
-      done();
-    }).catch((error)=>{
-      assert.ok(false, error);
-      done();
-    });
+    })
+      .then((result) => {
+        assert.equal(result, 'success');
+        done();
+      })
+      .catch((error) => {
+        assert.ok(false, error);
+        done();
+      });
   });
 
   test('throws errors, rejects promise then eventually passes', function (assert) {
     assert.expect(4);
     let done = assert.async();
     let count = 0;
-    retry((resolve, reject)=> {
-      count = count + 1;
-      if(count === 1){
-        assert.ok(true);
-        throw `count ${count}`;
-      }else if(count === 2){
-        assert.ok(true);
-        throw `count ${count}`;
-      }else if(count === 3){
-        assert.ok(true);
-        later(() => reject(`count ${count}`), 1);
-      }else if(count === 4){
-        resolve('my return');
+    retry(
+      (resolve, reject) => {
+        count = count + 1;
+        if (count === 1) {
+          assert.ok(true);
+          throw `count ${count}`;
+        } else if (count === 2) {
+          assert.ok(true);
+          throw `count ${count}`;
+        } else if (count === 3) {
+          assert.ok(true);
+          later(() => reject(`count ${count}`), 1);
+        } else if (count === 4) {
+          resolve('my return');
+        }
+      },
+      5,
+      () => {
+        return 2;
       }
-    }, 5, ()=>{return 2;}).then((result)=>{
-      assert.equal(result, 'my return');
-      done();
-    }).catch((error)=>{
-      assert.ok(false, `with error: ${error}`);
-      done();
-    });
+    )
+      .then((result) => {
+        assert.equal(result, 'my return');
+        done();
+      })
+      .catch((error) => {
+        assert.ok(false, `with error: ${error}`);
+        done();
+      });
   });
 
-  test('backoff delay count starts at 0 and increments by 1 until max', function(assert){
+  test('backoff delay count starts at 0 and increments by 1 until max', function (assert) {
     assert.expect(6);
     let done = assert.async();
     let count = 0;
-    retry(()=> {
-      count = count + 1;
-      throw `count ${count}`;
-      }, 5, (retry)=>{
-        assert.equal(retry, count-1);
+    retry(
+      () => {
+        count = count + 1;
+        throw `count ${count}`;
+      },
+      5,
+      (retry) => {
+        assert.equal(retry, count - 1);
         return 1;
       }
-    ).then(()=>{
-      assert.ok(false);
-      done();
-    }).catch(()=>{
-      assert.ok(true);
-      done();
-    });
-
+    )
+      .then(() => {
+        assert.ok(false);
+        done();
+      })
+      .catch(() => {
+        assert.ok(true);
+        done();
+      });
   });
 
-  module('conditionFnc', function() {
-
-    let fncToRetry = () => { throw count++ };
+  module('conditionFnc', function () {
+    let fncToRetry = () => {
+      throw count++;
+    };
     let maxRetries = 5;
     let delayFnc = () => 2;
     let count = 0;
 
-    test('does not retry if the condition fails', function(assert){
+    test('does not retry if the condition fails', function (assert) {
       assert.expect(2);
       let done = assert.async();
       count = 0;
       let conditionFnc = () => {
         assert.ok(true);
         return false;
-      }
+      };
 
-      retry(
-        fncToRetry,
-        maxRetries,
-        delayFnc,
-        conditionFnc
-      )
-      .then(() => done())
-      .catch((errorCount)=>{
-        assert.equal(errorCount, 0);
-        done();
-      });
+      retry(fncToRetry, maxRetries, delayFnc, conditionFnc)
+        .then(() => done())
+        .catch((errorCount) => {
+          assert.equal(errorCount, 0);
+          done();
+        });
     });
 
-    test('retries if the condition passes until maxRetries', function(assert){
+    test('retries if the condition passes until maxRetries', function (assert) {
       assert.expect(6);
       let done = assert.async();
       count = 0;
       let conditionFnc = () => {
         assert.ok(true);
         return true;
-      }
+      };
 
-      retry(
-        fncToRetry,
-        maxRetries,
-        delayFnc,
-        conditionFnc
-      )
-      .then(() => done())
-      .catch((errorCount)=>{
-        assert.equal(errorCount, 5);
-        done();
-      });
+      retry(fncToRetry, maxRetries, delayFnc, conditionFnc)
+        .then(() => done())
+        .catch((errorCount) => {
+          assert.equal(errorCount, 5);
+          done();
+        });
     });
 
-    test('retries if the condition passes until no failure', function(assert){
+    test('retries if the condition passes until no failure', function (assert) {
       assert.expect(3);
       let done = assert.async();
       count = 0;
@@ -178,19 +202,14 @@ module('Unit | Utility | retry', function() {
       let conditionFnc = () => {
         assert.ok(true);
         return true;
-      }
+      };
 
-      retry(
-        fncToRetry,
-        maxRetries,
-        delayFnc,
-        conditionFnc
-      )
-      .then(() => done())
-      .catch((errorCount)=>{
-        assert.equal(errorCount, 3);
-        done();
-      });
+      retry(fncToRetry, maxRetries, delayFnc, conditionFnc)
+        .then(() => done())
+        .catch((errorCount) => {
+          assert.equal(errorCount, 3);
+          done();
+        });
     });
   });
 });
